ffe5ec4d15fb872fc8acf144b0c68e7a175000c6
pchavan2021 <103229699+pchavan2021@users.noreply.github.com>
2023-11-28 11:24:13 -0500

    Update HashTest.java

--- HashTest.java
+++ HashTest.java
@@ -1,8 +1,8 @@
 import student.TestCase;
 
 /**
- * @author <Put something here>
- * @version <Put something here>
+ * @author pallucah21
+ * @version evanhowe03
  */
 public class HashTest extends TestCase {
     /**
