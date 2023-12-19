34002b99b8143743e9e4adda753cfee07641e5ee
pchavan2021 <103229699+pchavan2021@users.noreply.github.com>
2023-12-19 18:13:29 -0500

    Update GraphProject.java

--- GraphProject.java
+++ GraphProject.java
@@ -1,35 +1,4 @@
-// -------------------------------------------------------------------------
-/**
- * Main for Graph project (CS3114/CS5040 Fall 2023 Project 4).
- * Usage: java GraphProject <init-hash-size> <command-file>
- *
- * @author pallucha21 and evanhowe03
- * @version 1.0
- *
- */
 
-// On my honor:
-// - I have not used source code obtained from another current or
-// former student, or any other unauthorized source, either
-// modified or unmodified.
-//
-// - All source code and documentation used in my program is
-// either my original work, or was derived by me from the
-// source code published in the textbook for this course.
-//
-// - I have not discussed coding details about this project with
-// anyone other than my partner (in the case of a joint
-// submission), instructor, ACM/UPE tutors or the TAs assigned
-// to this course. I understand that I may discuss the concepts
-// of this program with other students, and that another student
-// may help me debug my program so long as neither of us writes
-// anything during the discussion or modifies any computer file
-// during the discussion. I have violated neither the spirit nor
-// letter of this restriction.
-
-/**
- * Project runner
- */
 public class GraphProject {
     /**
      * @param args
